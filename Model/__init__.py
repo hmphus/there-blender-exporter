66c458e23165d0af4b75a1d367fd150cc01d17f0
Brian Gontowski <brian@gontowski.com>
2022-07-19 09:55:17 +0900

    Bumped version

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'There Model format',
     'author': 'Brian Gontowski',
-    'version': (1, 2, 2),
+    'version': (1, 2, 3),
     'blender': (2, 93, 0),
     'location': 'File > Import-Export',
     'description': 'Export as Model for There.com',
